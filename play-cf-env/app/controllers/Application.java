c3b2ce52df612c6044171e1028efcc19f83f723d
Troy Astle <troy.astle@gmail.com>
2013-12-02 12:48:46 +0000

    added more config info

--- play-cf-env/app/controllers/Application.java
+++ play-cf-env/app/controllers/Application.java
@@ -1,18 +1,22 @@
 package controllers;
 
+import play.Play;
+import play.Logger;
+import play.Configuration;
 import play.mvc.*;
 
 import views.html.index;
 
 import java.util.*;
 import java.util.Map;
-import java.util.logging.Logger;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
 
 public class Application extends Controller {
 
-    private static Logger sLogger = Logger.getLogger(Application.class.getName());
     private static List<String> sEnvironmentVariableNames = null;
-
+    private static List<String> sPlayVariableNames = null;
 
     /**
      * @return The title of the app
@@ -37,6 +41,69 @@ public class Application extends Controller {
         return sEnvironmentVariableNames;
     }
 
+    public static List<String> getJVMRuntimeParameters() {
+        RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
+        return bean.getInputArguments();
+    }
+
+    public static List<String> getPlayConfigurationKeys() {
+        if (sPlayVariableNames == null) {
+            synchronized (Application.class) {
+                List<String> configKeys = new ArrayList<String>();
+                configKeys.addAll(Play.application().configuration().keys());
+                Collections.sort(configKeys);
+                sPlayVariableNames = configKeys;
+            }
+        }
+        return sPlayVariableNames;
+    }
+
+    public static String getPrintablePlayVariable(String pKey) {
+
+        String printableValue  = null;
+
+        try {
+            printableValue = Play.application().configuration().getString(pKey);
+        } catch (Exception e) {
+            Logger.info("Could not read config value "+ pKey +" as String");
+        }
+
+        if (printableValue == null) {
+            try {
+                List<Object> listValues = Play.application().configuration().getList(pKey);
+                StringBuilder buff = new StringBuilder();
+                for (Object obj : listValues) {
+                    if (buff.length() > 0) {
+                        buff.append(", ");
+                    }
+                    buff.append(obj);
+                }
+                printableValue = buff.toString();
+            } catch (Exception e) {
+                Logger.info("Could not read config value "+ pKey +" as List");
+            }
+        }
+
+        if (printableValue == null) {
+            Map <String,Object> config = Play.application().configuration().asMap();
+            Object configObj = config.get(pKey);
+
+            if (configObj != null) {
+
+                Logger.info("Config object " + pKey + " has type " + configObj.getClass().getCanonicalName());
+
+                if (configObj instanceof String) {
+                    printableValue = (String)configObj;
+                } else {
+                    printableValue = configObj.toString();
+                }
+            }
+        }
+
+        return printableValue;
+    }
+
+
     /**
      * @return The system environment variable map
      */
@@ -48,7 +115,7 @@ public class Application extends Controller {
      * @return page content
      */
     public static Result index() {
-        sLogger.info("Rendering with environment: " + getEnvironment());
+        Logger.info("Rendering with environment: " + getEnvironment());
         return ok(index.render());
     }
 
